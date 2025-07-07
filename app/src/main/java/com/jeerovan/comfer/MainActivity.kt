c319893bc0b7f2f216de2394d1e8022d080dfc2e
Jeevan Singh <tojeevansingh@gmail.com>
2025-07-07 17:42:46 +0530

    fixed maxWidth,maxHeight as dp pixels.

--- app/src/main/java/com/jeerovan/comfer/MainActivity.kt
+++ app/src/main/java/com/jeerovan/comfer/MainActivity.kt
@@ -2,31 +2,45 @@ package com.jeerovan.comfer
 
 import android.content.Intent
 import android.content.pm.ResolveInfo
+import android.content.res.Resources
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.produceState
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
@@ -56,7 +70,7 @@ class MainActivity : ComponentActivity() {
 fun LauncherScreen() {
     val context = LocalContext.current
     val packageManager = context.packageManager
-    var scrollOffset by remember { mutableStateOf(0f) }
+    var scrollOffset by remember { mutableFloatStateOf(0f) }
 
     val apps by produceState<List<AppInfo>>(initialValue = emptyList()) {
         val intent = Intent(Intent.ACTION_MAIN, null).addCategory(Intent.CATEGORY_LAUNCHER)
@@ -104,9 +118,6 @@ fun LauncherScreen() {
 
 @Composable
 fun UshapedAppList(apps: List<AppInfo>, scrollOffset: Float) {
-    val context = LocalContext.current
-    val packageManager = context.packageManager
-
     val numVisibleIcons = 40
     val numTopIcons = 11
     val numSideIcons = (numVisibleIcons - numTopIcons) / 2
@@ -118,74 +129,87 @@ fun UshapedAppList(apps: List<AppInfo>, scrollOffset: Float) {
     val scrollIndex = (scrollOffset / 20f).roundToInt()
     val startIndex = (scrollIndex - numVisibleIcons / 2 + totalIcons) % totalIcons
 
-    Layout(
-        content = {
-            for (i in 0 until numVisibleIcons) {
-                val appIndex = (startIndex + i) % totalIcons
-                val isCenter = i == numSideIcons + numTopIcons / 2
-                val iconSize = if (isCenter) largeIconSize else smallIconSize
+    BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
 
-                Box(
-                    modifier = Modifier
-                        .size(iconSize)
-                        .clip(CircleShape)
-                        .background(Color.White)
-                        //.border(2.dp, Color.Black, CircleShape)
-                        .clickable {
-                            val launchIntent =
-                                packageManager.getLaunchIntentForPackage(apps[appIndex].resolveInfo.activityInfo.packageName)
-                            context.startActivity(launchIntent)
-                        },
-                    contentAlignment = Alignment.Center
-                ) {
-                    Image(
-                        painter = rememberDrawablePainter(drawable = apps[appIndex].icon),
-                        contentDescription = apps[appIndex].label.toString(),
-                        modifier = Modifier.padding(4.dp)
-                    )
-                }
+        val density = LocalDensity.current
+        val width =  with(density){maxWidth.value.dp.toPx()}
+        val height = with(density){maxHeight.value.dp.toPx()}
+        val sidePadding = with(density) { 32.dp.toPx()}
+        val topPadding = with(density) { 64.dp.toPx() }
+
+        val verticalSpacing = (height - topPadding * 2) / (numSideIcons - 1)
+        val arcRadius = (width - sidePadding * 2) / 2
+        val angularSpacing = PI / (numTopIcons - 1)
+
+        for (i in 0 until numVisibleIcons) {
+            val appIndex = (startIndex + i) % totalIcons
+            val isCenter = i == numSideIcons + numTopIcons / 2
+
+            val (x, y) = if (i < numSideIcons) {
+                // Left side
+                val xPos = sidePadding
+                val yPos = arcRadius + verticalSpacing + height - topPadding - i * verticalSpacing
+                Pair(xPos, yPos)
+            } else if (i < numSideIcons + numTopIcons) {
+                // Top arc
+                val arcIndex = i - numSideIcons
+                val angle = PI - arcIndex * angularSpacing
+                val xPos = width / 2 + arcRadius * cos(angle).toFloat()
+                val yPos = topPadding + arcRadius * (1 - sin(angle)).toFloat()
+                Pair(xPos, yPos)
+            } else {
+                // Right side
+                val sideIndex = i - numSideIcons - numTopIcons
+                val xPos = width - sidePadding
+                val yPos = arcRadius + verticalSpacing + topPadding + sideIndex * verticalSpacing
+                Pair(xPos, yPos)
             }
-        }
-    ) { measurables, constraints ->
-        val placeables = measurables.map { it.measure(Constraints()) }
 
-        layout(constraints.maxWidth, constraints.maxHeight) {
-            val width = constraints.maxWidth.toFloat()
-            val height = constraints.maxHeight.toFloat()
-
-            val sidePadding = 32.dp.toPx()
-            val topPadding = 64.dp.toPx()
-
-            val verticalSpacing = (height - topPadding * 2) / (numSideIcons - 1)
-            val arcRadius = (width - sidePadding * 2) / 2
-            val angularSpacing = PI / (numTopIcons - 1)
-
-            placeables.forEachIndexed { i, placeable ->
-                val (x, y) = if (i < numSideIcons) {
-                    // Left side
-                    val xPos = sidePadding
-                    val yPos = arcRadius + verticalSpacing + height - topPadding - i * verticalSpacing
-                    Pair(xPos, yPos)
-                } else if (i < numSideIcons + numTopIcons) {
-                    // Top arc
-                    val arcIndex = i - numSideIcons
-                    val angle = PI - arcIndex * angularSpacing
-                    val xPos = width / 2 + arcRadius * cos(angle).toFloat()
-                    val yPos = topPadding + arcRadius * (1 - sin(angle)).toFloat()
-                    Pair(xPos, yPos)
-                } else {
-                    // Right side
-                    val sideIndex = i - numSideIcons - numTopIcons
-                    val xPos = width - sidePadding
-                    val yPos = arcRadius +  verticalSpacing + topPadding + sideIndex * verticalSpacing
-                    Pair(xPos, yPos)
-                }
-
-                placeable.placeRelative(
-                    x = (x - placeable.width / 2).toInt(),
-                    y = (y - placeable.height / 2).toInt()
-                )
-            }
+            AppIcon(
+                app = apps[appIndex],
+                x = x,
+                y = y,
+                isCenter = isCenter,
+                smallIconSize = smallIconSize,
+                largeIconSize = largeIconSize
+            )
         }
     }
+}
+
+@Composable
+fun AppIcon(app: AppInfo, x: Float, y: Float, isCenter: Boolean, smallIconSize: Dp, largeIconSize: Dp) {
+    val context = LocalContext.current
+    val packageManager = context.packageManager
+
+    val animatedSize by animateDpAsState(
+        targetValue = if (isCenter) largeIconSize else smallIconSize,
+        animationSpec = tween(300)
+    )
+    val animatedX by animateDpAsState(targetValue = x.toDp(), animationSpec = tween(300))
+    val animatedY by animateDpAsState(targetValue = y.toDp(), animationSpec = tween(300))
+
+    Box(
+        modifier = Modifier
+            .offset(x = animatedX - animatedSize / 2, y = animatedY - animatedSize / 2)
+            .size(animatedSize)
+            .clip(CircleShape)
+            .background(Color.White)
+            .clickable {
+                val launchIntent =
+                    packageManager.getLaunchIntentForPackage(app.resolveInfo.activityInfo.packageName)
+                context.startActivity(launchIntent)
+            },
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = rememberDrawablePainter(drawable = app.icon),
+            contentDescription = app.label.toString(),
+            modifier = Modifier.padding(4.dp)
+        )
+    }
+}
+
+private fun Float.toDp(): Dp {
+    return (this / Resources.getSystem().displayMetrics.density).dp
 }
