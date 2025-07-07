1771fcf4511c8588dd9ccaab6dcc95c528305f30
Jeevan Singh <tojeevansingh@gmail.com>
2025-07-07 09:37:42 +0530

    top app bar with background image

--- app/src/main/java/com/jeerovan/comfer/MainActivity.kt
+++ app/src/main/java/com/jeerovan/comfer/MainActivity.kt
@@ -1,47 +1,92 @@
 package com.jeerovan.comfer
 
+import android.content.Intent
+import android.content.pm.ResolveInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import com.jeerovan.comfer.ui.theme.ComferTheme
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ComferTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            LauncherScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun LauncherScreen() {
+    val context = LocalContext.current
+    val packageManager = context.packageManager
+    val intent = Intent(Intent.ACTION_MAIN, null).apply {
+        addCategory(Intent.CATEGORY_LAUNCHER)
+    }
+    val apps = packageManager.queryIntentActivities(intent, 0)
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = rememberAsyncImagePainter(
+                ImageRequest.Builder(LocalContext.current)
+                    .data("https://images.unsplash.com/photo-1611656993299-9f9d73232b9c?w=2000&q=99")
+                    .crossfade(true)
+                    .build()
+            ),
+            contentDescription = "Background",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+        Column {
+            Spacer(modifier = Modifier.height(12.dp))
+            LazyRow(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                items(apps) { app ->
+                    AppIcon(app)
+                }
+            }
+        }
+
+    }
+}
+
+@Composable
+fun AppIcon(app: ResolveInfo) {
+    val context = LocalContext.current
+    val packageManager = context.packageManager
+    val launchIntent = packageManager.getLaunchIntentForPackage(app.activityInfo.packageName)
+
+    Image(
+        painter = rememberAsyncImagePainter(app.loadIcon(packageManager)),
+        contentDescription = app.loadLabel(packageManager).toString(),
+        modifier = Modifier
+            .size(64.dp)
+            .padding(8.dp)
+            .clickable {
+                context.startActivity(launchIntent)
+            }
     )
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
-    ComferTheme {
-        Greeting("Android")
-    }
+fun PreviewLauncher(){
+    LauncherScreen()
 }
