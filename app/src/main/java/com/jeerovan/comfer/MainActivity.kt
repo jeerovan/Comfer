f04586867c1b8ca4bbc115ac1e7c2a7c76af6616
Jeevan Singh <tojeevansingh@gmail.com>
2025-10-06 14:49:09 +0530

    Removed edge to edge call.

--- app/src/main/java/com/jeerovan/comfer/MainActivity.kt
+++ app/src/main/java/com/jeerovan/comfer/MainActivity.kt
@@ -270,7 +270,6 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
                 mainViewModel.onBackButtonPressed()
