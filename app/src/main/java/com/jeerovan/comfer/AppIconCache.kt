c6069ed36ce4231a5bb95a4074f8c7c53d26ec8b
Jeevan Singh <tojeevansingh@gmail.com>
2026-06-20 10:45:41 +0530

    fixed appicon cache size.

--- app/src/main/java/com/jeerovan/comfer/AppIconCache.kt
+++ app/src/main/java/com/jeerovan/comfer/AppIconCache.kt
@@ -1,14 +1,26 @@
 package com.jeerovan.comfer
 
 import android.graphics.drawable.Drawable
+import android.util.LruCache
 
 object AppIconCache {
-    private val iconCache = mutableMapOf<String, Drawable>()
+    private const val CACHE_SIZE = 100
+    
+    private val iconCache = object : LruCache<String, Drawable>(CACHE_SIZE) {
+        override fun sizeOf(key: String, value: Drawable): Int {
+            return 1
+        }
+    }
+
     fun getIcon(packageName: String): Drawable? {
-        return iconCache[packageName]
+        return iconCache.get(packageName)
     }
 
     fun cacheIcon(packageName: String, icon: Drawable) {
-        iconCache[packageName] = icon
+        iconCache.put(packageName, icon)
+    }
+    
+    fun clearCache() {
+        iconCache.evictAll()
     }
 }
